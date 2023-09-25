f0c946704447c14616fa620f812d58d31f350447
ncar285 <carlier.nicholas@gmail.com>
2023-09-25 11:07:33 -0700

    render loading splash while loading

--- frontend/src/components/Splash/Splash.jsx
+++ frontend/src/components/Splash/Splash.jsx
@@ -1,9 +1,10 @@
 import './Splash.css'
 import LoadingItem from './LoadingItem'
+import LoadingSplash from '../LoadingSplash/LoadingSplash'
 import { useDispatch, useSelector } from "react-redux"
 import { getListings, selectListings } from "../../store/listingsReducer"
 import ListingItem from "./ListingItem.jsx"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { SearchContext } from '../../App'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 // import { fetchListings } from '../../utils/listingApiUtils'
@@ -11,10 +12,10 @@ import { BsFillMapFill } from 'react-icons/bs'
 
 const Splash = () => {
 
+  const [isLoading, setIsLoading] = useState(true);
   const listings = useSelector(selectListings)
   const dispatch = useDispatch();
   const { searchParams }  = useContext(SearchContext);
-
   const history = useHistory();
 
   const mapMode = () => {
@@ -22,14 +23,22 @@ const Splash = () => {
   }
   
 
-
   useEffect(() => {
-    if (searchParams.search){
-      dispatch(getListings({ search: searchParams.search }));
-    } else {
-      dispatch(getListings())
-    }
-  },[])
+    const fetchData = async () => {
+      if (searchParams.search){
+        await dispatch(getListings({ search: searchParams.search }));
+      } else {
+        await dispatch(getListings());
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingSplash/>
+  }
 
   return (
     <>
