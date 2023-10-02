c3cac42b6a6523ebdd9dc888ccbe2f26a595d188
ncar285 <carlier.nicholas@gmail.com>
2023-10-02 00:23:34 -0700

    photo album clickable

--- frontend/src/components/PhotoAlbum/PhotoAlbum.jsx
+++ frontend/src/components/PhotoAlbum/PhotoAlbum.jsx
@@ -6,6 +6,15 @@ import SlideShow from '../SlideShow/SlideShow'
 const PhotoAlbum = ({ listing }) => {
 
     const [slideShowOpen, setSlideShowOpen] = useState(false)
+    const [slidePhoto, setSlidePhoto] = useState(null)
+
+    const overlay = (ind) => {
+    return <div className='img-overlay' 
+        onClick = {() => {
+            setSlideShowOpen(true)
+            setSlidePhoto(ind)}
+        }></div> 
+    }
 
     return (
 
@@ -14,27 +23,33 @@ const PhotoAlbum = ({ listing }) => {
         { slideShowOpen && 
             <SlideShow photos={listing.photoUrls} 
             setSlideShowOpen={setSlideShowOpen}
+            slidePhoto = {slidePhoto}
             />
         }
         
         <div className="photo-album">
             <div className='LHS-show'>
+                {overlay(0)}
                 <img className='main-show-image' src={listing.photoUrls[0]}  alt="" />
             </div>
             <div className='RHS-show'>
                 <div className='RHS-upper'>
                     <div className='album-1 RHS-img'>
+                        {overlay(1)}
                         <img className='RHS-image-1' src={listing.photoUrls[1]} alt="" />
                     </div>
                     <div className='album-2 RHS-img'> 
+                        {overlay(2)}
                         <img src={listing.photoUrls[2]}  alt="" />
                     </div>
                 </div>
                 <div className='RHS-lower'>
                     <div className='album-3 RHS-img'> 
+                        {overlay(3)}
                         <img src={listing.photoUrls[3]}  alt="" />
                     </div>
                     <div className='album-4 RHS-img'>
+                        {overlay(4)}
                         <img src={listing.photoUrls[4]}  alt="" />
                     </div>
                 </div>
--- frontend/src/components/SlideShow/SlideShow.jsx
+++ frontend/src/components/SlideShow/SlideShow.jsx
@@ -4,53 +4,44 @@ import {RxCross2} from 'react-icons/rx'
 import {AiOutlineLeft} from 'react-icons/ai'
 import {AiOutlineRight} from 'react-icons/ai'
 
-const SlideShow = ({photos, setSlideShowOpen}) => {
+const SlideShow = ({photos, setSlideShowOpen, slidePhoto}) => {
 
-    const [photoIndex, setPhotoIndex] = useState(1)
+    const initial = slidePhoto ? slidePhoto + 1 : 1
+    const [photoIndex, setPhotoIndex] = useState(initial)
 
     return (
-        <div className='slideshow-container'>
+        <div className='fullscreen-slideshow-container'>
             <div className='fixed-subcontainer'>
 
-                
+                <div className='close-slideshow'
+                onClick = {() => setSlideShowOpen(false)}
+                >
+                    <RxCross2 id='cross-icon-slideshow'/>
+                    <p> Close</p>
+                </div>
 
-                    <div className='close-slideshow'
-                    onClick = {() => setSlideShowOpen(false)}
+                <p id="slideshow-index">{photoIndex} / {photos.length}</p>
+
+                <div className='slideshow-image-container'>
+                    <img src={photos[photoIndex - 1]}  alt="" />
+                </div>
+
+                { photoIndex !== 1 &&
+                    <div className='slideshow-arrow left'
+                    onClick={()=>{setPhotoIndex(ind => ind - 1)}}
                     >
-                        <RxCross2 id='cross-icon-slideshow'/>
-                        <p> Close</p>
+                        <AiOutlineLeft/>
                     </div>
+                }
 
-                    <p id="slideshow-index">{photoIndex} / {photos.length}</p>
-                    {/* <p id="slideshow-index">hellow</p> */}
-
-                    <div className='slideshow-image-container'>
-                        <img src={photos[photoIndex - 1]}  alt="" />
+                { photoIndex !== photos.length &&
+                    <div className='slideshow-arrow right'
+                    onClick={()=>{setPhotoIndex(ind => ind + 1)}}>
+                        <AiOutlineRight/>
                     </div>
-
-                    { photoIndex !== 1 &&
-                        <div className='slideshow-arrow left'
-                        onClick={()=>{setPhotoIndex(ind => ind - 1)}}
-                        >
-                            <AiOutlineLeft/>
-                        </div>
-                    }
-
-                    { photoIndex !== photos.length &&
-                        <div className='slideshow-arrow right'
-                        onClick={()=>{setPhotoIndex(ind => ind + 1)}}>
-                            <AiOutlineRight/>
-                        </div>
-                    }
-
-                
-
-        
-
-
+                }
 
             </div>
-
 
         </div>
     )
